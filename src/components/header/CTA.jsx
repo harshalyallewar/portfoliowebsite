0bba798aaddafbe6c785a768efa76b8c4ac04844
harshalyallewar <harshalyallewar2@gmail.com>
2023-10-05 00:03:48 +0530

    links updated

--- src/components/header/CTA.jsx
+++ src/components/header/CTA.jsx
@@ -3,7 +3,10 @@ import React from "react";
 const CTA = () => {
   return (
     <div className="cta">
-      <a href="#" className="btn">
+      <a target="_blank"
+        href="https://drive.google.com/file/d/1f3EL_RS8n4-O3ToItJiOLvGyCOHLczqC/view?usp=sharing"
+        className="btn"
+      >
         Download CV
       </a>
       <a href="#contact" className="btn btn-primary">
